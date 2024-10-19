60d6695f889b7903bd678fc5655f81dbcca93ff2
Iqbal <iqbalrahmat27@gmail.com>
2024-10-19 15:42:35 +0700

    Pesan commit

---
+++ pitchingfest-main/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      animation: {
+        logo: "logo 2.5s ease-in-out infinite",
+      },
+      keyframes: {
+        logo: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      }
+    },
+  },
+  plugins: [],
+}
+
